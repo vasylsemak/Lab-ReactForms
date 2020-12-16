f98b2e86c44fb0b0e0b4b32ec5e717ddb48a137a
Vasyl Semak <vasylsmk1401@gmail.com>
2020-12-15 21:27:11 -0500

    create: clearInput f-n to clean input fields after submition

--- app/index.js
+++ app/index.js
@@ -4,16 +4,24 @@ import ReactDOM from 'react-dom'
 class Form extends React.Component {
   constructor() {
     super();
-    this.state = { username: ''};
+    this.state = {
+      username: '',
+      password: ''
+    }
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.clearInput = this.clearInput.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const username = event.target.username.value;
-    this.setState({ username });
+    let username = event.target.username.value;
+    let password = event.target.password.value;
+    this.setState({ username, password });
+
+    console.log('State ->', this.state);
+    this.clearInput();
   }
 
   handleChange(event) {
@@ -22,8 +30,11 @@ class Form extends React.Component {
     this.setState({ [name]: value });
   }
 
+  clearInput() {
+    this.setState({ username: '', password: '' });
+  }
+
   render () {
-    console.log('state ->', this.state);
     return (
       <div id='container'>
         <div id='navbar'>
@@ -31,7 +42,19 @@ class Form extends React.Component {
         </div>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor='username'>Username:</label>
-          <input type='text' name='username' value={this.state.username} onChange={this.handleChange} />
+          <input
+            type='text'
+            name='username'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='text'
+            name='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
           <button type='submit'>Submit</button>
         </form>
       </div>
